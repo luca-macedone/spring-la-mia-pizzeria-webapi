9b44c30ebb59ccf0908e07bef6fa0636c7a36d42
luca-macedone <luca.macedone@outlook.it>
2023-10-17 12:51:37 +0200

    add frontend initial files

---
+++ frontend/src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import { ref } from 'vue';
+
+
+// const onMounted = onMounted({
+
+// });
+
+const data = ref({
+	hello: "hello world",
+});
+
+</script>
+
+<template>
+	<div>
+		<h1>Test {{ data.hello }}</h1>
+	</div>
+</template>
+
+<style lang="scss" scoped></style>
---
+++ frontend/src/main.js
@@ -0,0 +1,6 @@
+import './assets/sass/common.scss'
+import "bootstrap/dist/css/bootstrap.css";
+import { createApp } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
